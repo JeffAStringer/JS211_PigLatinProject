b292ac09d67d701a63430777be463068ee46e8fe
JeffAStringer <jastringer@icloud.com>
2020-12-07 21:56:28 -0600

    class work

--- main.js
+++ main.js
@@ -11,12 +11,33 @@ const rl = readline.createInterface({
 });
 
 
-const pigLatin = (word) => {
+const pigLatin = (str) => {
+  // Convert string to lowercase
+  str = str.toLowerCase().trim()
+  // Initialize array of vowels
+  const vowels = ["a", "e", "i", "o", "u"];
+  // Initialize vowel index to 0
+  let vowelIndex = 0;
 
-  // Your code here
-
+  if (vowels.includes(str[0])) {
+    // If first letter is a vowel
+    return str + "yay";
+  } else {
+    // If the first letter isn't a vowel i.e is a consonant
+    for (let char of str) {
+      // Loop through until the first vowel is found
+      if (vowels.includes(char)) {
+        // Store the index at which the first vowel exists
+        vowelIndex = str.indexOf(char);
+        break;
+      }
+    }
+    // Compose final string
+    return str.slice(vowelIndex) + str.slice(0,vowelIndex) + "ay";
+  }
 }
 
+ 
 // the first function called in the program to get an input from the user
 // to run the function use the command: node main.js
 // to close it ctrl + C
